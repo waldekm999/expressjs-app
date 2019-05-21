249ca4d6b7250f2483add16fc44d04722cd3c11d
waldekm999 <waldekm999@hotmail.com>
2019-05-21 21:09:29 +0200

    new sites Info added

--- source/app.js
+++ source/app.js
@@ -17,6 +17,11 @@ app.get('/services', function(req, res){
     console.log('Zażądano strony Services');
 });
 
+app.get('/info', function(req, res){
+    res.send('DevOps - strona Info!');
+    console.log('Zażądano strony Info');
+});
+
 app.listen(3001, function(){
 	console.log('Aplikacja OK');
 }); 
